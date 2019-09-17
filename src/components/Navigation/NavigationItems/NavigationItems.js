c34ff9988dfed46bbae49b070cfebd8f017b790f
Ebaboo <vintorez00@gmail.com>
2019-09-17 23:45:25 +0300

    sidedrawer icons added + edit event user UI protection added

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -3,6 +3,11 @@ import NavigationItem from "./NavigationItem/NavigationItem";
 
 import classes from "./NavigationItems.module.css";
 import Aux from "../../../hoc/Auxillary/Auxillary";
+import loginImg from "../../../assests/login.png";
+import signupImg from "../../../assests/singup.png";
+import eventsImg from "../../../assests/event_menu_icon.png";
+import createEventImg from "../../../assests/ctrate_menu_icon.png";
+import logoutImg from "../../../assests/logout_menu-icon.png";
 
 const NavigationItems = props => {
   return (
@@ -10,16 +15,29 @@ const NavigationItems = props => {
       <nav className={classes.NavigationItems}>
         {!props.isAuth ? (
           <Aux>
-            <NavigationItem link="/login">Login</NavigationItem>
-            <NavigationItem link="/signup">Signup</NavigationItem>
+            <NavigationItem link="/login">
+                <img src={loginImg} alt="menuICon" />
+                <span>Login</span>
+            </NavigationItem>
+            <NavigationItem link="/signup">
+              <img src={signupImg} alt="menuICon" />
+                <span>Signup</span>
+            </NavigationItem>
           </Aux>
         ) : (
           <Aux>
-            <NavigationItem link="/">Home</NavigationItem>
-            <NavigationItem link="/events/create-event">
-              Create Event
+            <NavigationItem link="/allEvents">
+            <img src={eventsImg} alt="menuICon" />
+                <span>Events</span>
             </NavigationItem>
-            <NavigationItem link="/logout">Logout</NavigationItem>
+            <NavigationItem link="/events/create-event">
+            <img src={createEventImg} alt="menuICon" />
+                <span>Create Event</span>
+            </NavigationItem>
+            <NavigationItem link="/logout">
+              <img src={logoutImg} alt="menuICon" />
+                <span>Logout</span>
+            </NavigationItem>
           </Aux>
         )}
       </nav>
